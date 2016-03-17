4c060e10da43f1ebb655074e06eea1172a92f289
Oksana Boyarko <brumeregan@gmail.com>
2016-03-17 17:56:24 +0200

    changed js array file. deleted for loop => used indexOf method

--- js_1/js/hw_02-array-push.js
+++ js_1/js/hw_02-array-push.js
@@ -4,21 +4,13 @@ function auth(){
 	var array = [];
 
 	for (var i = 0; i < 5; i++){
-		var nameToAdd = prompt("Please, enter name:");
+		var nameToAdd = prompt("Please, some enter name:");
 		array.push(nameToAdd);
 	}
 
-	var user = prompt("Enter username");
-
-	isAdmin = false;
-
-	for (var i = 0; i < array.length; i++){
-		if(user.toUpperCase() == array[i].toUpperCase()){
-			isAdmin = true;
-		}
-	}
-
-	if(isAdmin){
+	var user = prompt("Enter UserName");
+	
+	if( array.indexOf(user) >= 0){
 		return alert(user + ", Вы успешно вошли в систему!");
 	}
 

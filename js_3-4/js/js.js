50ba9f11b6e82b34028f26bda2fdb36644ab350d
Oksana Boyarko <brumeregan@gmail.com>
2016-03-20 22:14:15 +0200

    Added final js homework 3-4

--- js_3-4/js/js.js
+++ js_3-4/js/js.js
@@ -1,34 +1,49 @@
 var generateHtml = {
-	body: document.querySelector('body'),
-	title: "Заголовок теста",
+	title: "Тест по программированию",
 	questions: {
-		"Вопрос 1": ["Ответ 1", "Ответ 2", "Ответ 3"],
-		"Вопрос 2": ["Ответ 1", "Ответ 2", "Ответ 3"],
-		"Вопрос 3": ["Ответ 1", "Ответ 2", "Ответ 3"]
+		"Вопрос №1": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
+		"Вопрос №2": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
+		"Вопрос №3": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]
 	},
-	
+
 	generate: function(){
-		this.createTitle();
-		this.createBox();
-		this.createButton();
+		var container = this.createContainer();
+		container.appendChild(this.createTitle());
+		var boxes = this.createBoxes();
+		boxes.appendChild(this.createButton());
+		container.appendChild(boxes);
+		document.querySelector("body").appendChild(container);
+	},
+
+	createContainer: function(){
+		var container = document.createElement('div');
+		container.classList.add('container');
+		return container;
 	},
 
 	createTitle: function(){
 		var title = document.createElement('h1');
+		title.classList.add("text-center");
 		title.innerHTML = this.title;
-		this.body.appendChild(title);
+		return title;
 	},
 
-	createBox: function(){
-		var box = document.createElement('div');
-		var keyId = 0;
+	createBoxes: function(){
+		var keyId = 1;
+		var row = document.createElement("div");
+		row.classList.add("col-lg-6", "col-lg-offset-3");
+		
+
 		for (var key in this.questions ){
+			var questionBox = document.createElement('div');
 			var questionElem = document.createElement('h3');
 			questionElem.classList.add('question');
-			questionElem.innerHTML = key;
-			box.appendChild(questionElem);
+			questionElem.innerHTML = keyId + '. ' + key;
+			questionBox.appendChild(questionElem);
 
 			for (var i = 0; i < this.questions[key].length; i++){
+				var box = document.createElement('div');
+				box.classList.add("checkbox");
 
 				var answer = this.questions[key][i];
 
@@ -40,21 +55,27 @@ var generateHtml = {
 				label.setAttribute('for', 'checkbox_' + keyId + "_" + i);
 				label.innerHTML = answer;
 
-				box.appendChild(checkbox);
+				label.insertBefore(checkbox, label.firstChild);
+
 				box.appendChild(label);
+				questionBox.appendChild(box);
+
 			}
-			this.body.appendChild(box);
+			
 			keyId++;
+			questionBox.appendChild(document.createElement("hr"));
+			row.appendChild(questionBox);
+			
 		}
+		return row;
 	},
 
 	createButton: function(){
 		var button = document.createElement('button');
-		button.innerHTML = "Кнопка. Нажать здесь";
-		this.body.appendChild(button);
+		button.classList.add("center-block", "btn", "btn-primary");
+		button.innerHTML = "Проверить мои результаты";
+		return button;
 	}
 }
 
 generateHtml.generate();
-
-

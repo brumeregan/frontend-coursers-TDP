57ea305a323f26cb93e8e61f0da8889055baefa5
Oksana Boyarko <brumeregan@gmail.com>
2016-04-08 16:17:25 +0300

    Refactoring code. added function createElemenet, quest array etc

--- js_3-4/js/js.js
+++ js_3-4/js/js.js
@@ -1,80 +1,126 @@
+'use strict';
+
 var generateHtml = {
-	title: "Тест по программированию",
-	questions: {
-		"Вопрос №1": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
-		"Вопрос №2": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
-		"Вопрос №3": ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]
+	titleQuest: "Тест по программированию",
+	questions: [
+		{
+			title: "Вопрос №1",
+			answers: [
+				{
+					text: "Вариант ответа №1",
+					correct: true
+				},
+				{
+					text: "Вариант ответа №2",
+					correct: false
+				},
+				{
+					text: "Вариант ответа №3",
+					correct: false
+				}
+			]
+		},
+		{
+			title: "Вопрос №2",
+			answers: [
+				{
+					text: "Вариант ответа №2-1",
+					correct: true
+				},
+				{
+					text: "Вариант ответа №2-2",
+					correct: false
+				},
+				{
+					text: "Вариант ответа №2-3",
+					correct: false
+				}
+			]
+		},
+		{
+			title: "Вопрос №3",
+			answers: [
+				{
+					text: "Вариант ответа №1",
+					correct: true
+				},
+				{
+					text: "Вариант ответа №2",
+					correct: false
+				},
+				{
+					text: "Вариант ответа №3",
+					correct: false
+				}
+			]
+		}
+	],
+
+	init: function(){
+		var container = document.body.appendChild(this.createContainer());
+		var row = container.appendChild(this.createRow());
+		var col = row.appendChild(this.createCol());
+		col.appendChild(this.createTitle());
+		col.appendChild(this.createQuestion());
+		col.appendChild(this.createButton());
 	},
 
-	generate: function(){
-		var container = this.createContainer();
-		container.appendChild(this.createTitle());
-		var boxes = this.createBoxes();
-		boxes.appendChild(this.createButton());
-		container.appendChild(boxes);
-		document.querySelector("body").appendChild(container);
+	createQuestion: function () {
+		var quest__container = this.createElement('div', 'quest__container');
+		for (var i = 0; i < this.questions.length; i++) {
+			var quest__item = this.createElement('div');
+			var questTitle = this.createElement('h3', 'quest__title', this.questions[i].title);
+			quest__item.appendChild(questTitle);
+			quest__container.appendChild(quest__item);
+			for (var k = 0; k < this.questions[i].answers.length; k++) {
+				var num = +i + 1 + '_' + (+k + 1);
+				var box = this.createElement('div', 'checkbox', null);
+				var labelElem = this.createElement('label', null, this.questions[i].answers[k].text);
+				var inputElem = this.createElement('input');
+				inputElem.type = 'checkbox';
+				inputElem.setAttribute('id', 'checkbox_' + num);
+				labelElem.insertAdjacentElement('afterBegin', inputElem);
+				box.insertAdjacentElement('beforeEnd', labelElem);
+				quest__item.appendChild(box);
+			}
+			var line = this.createElement('hr');
+			quest__item.insertAdjacentElement('afterEnd', line);
+		}
+		return quest__container;
+	},
+
+	createElement: function (tag, className, text) {
+		var elem = document.createElement(tag);
+		if (className) elem.className = className;
+		if (text) elem.innerHTML = text;
+		return elem;
 	},
 
 	createContainer: function(){
-		var container = document.createElement('div');
-		container.classList.add('container');
+		var container = this.createElement('div', 'container');
 		return container;
 	},
 
-	createTitle: function(){
-		var title = document.createElement('h1');
-		title.classList.add("text-center");
-		title.innerHTML = this.title;
-		return title;
+	createRow: function(){
+		var row = this.createElement('div', 'row');
+		return row;
 	},
 
-	createBoxes: function(){
-		var keyId = 1;
-		var col = document.createElement("div");
-		col.classList.add("col-lg-6", "col-lg-offset-3");
-		
-		for (var key in this.questions){
-			var questionBox = document.createElement('div');
-			var questionElem = document.createElement('h3');
-			questionElem.classList.add('question');
-			questionElem.innerHTML = keyId + '. ' + key;
-			questionBox.appendChild(questionElem);
-
-			for (var i = 0; i < this.questions[key].length; i++){
-				var box = document.createElement('div');
-				box.classList.add("checkbox");
-
-				var answer = this.questions[key][i];
-
-				var checkbox = document.createElement('input');
-				checkbox.type = 'checkbox';
-				checkbox.setAttribute('id', 'checkbox_' + keyId + "_" + i);
-
-				var label = document.createElement('label');
-				label.setAttribute('for', 'checkbox_' + keyId + "_" + i);
-				label.innerHTML = answer;
-
-				label.insertBefore(checkbox, label.firstChild);
-
-				box.appendChild(label);
-				questionBox.appendChild(box);
-
-			}
-			
-			keyId++;
-			questionBox.appendChild(document.createElement("hr"));
-			col.appendChild(questionBox);
-			
-		}
+	createCol: function(){
+		var col = this.createElement('div', 'col-md-6 col-md-offset-3');
 		return col;
 	},
 
-	createButton: function(){
-		var button = document.createElement('button');
-		button.classList.add("center-block", "btn", "btn-primary");
-		button.innerHTML = "Проверить мои результаты";
+	createTitle: function(){
+		var title = this.createElement('h2', 'text-center', this.titleQuest);
+		return title;
+	},
+
+	createButton: function () {
+		var button = this.createElement('button', 'btn btn-primary center-block', 'Проверить мои результаты');
 		return button;
 	}
 };
 
-generateHtml.generate();
+
+generateHtml.init();
